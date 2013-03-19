b9a419327cf44bd8b12f4faaeb4c7abfb7585500
Dominic Graefen <dominic.graefen@gmail.com>
2013-03-19 23:28:56 +0100

    Adds enable_tap_to_click

---
+++ manifests/global/enable_tap_to_click.pp
@@ -0,0 +1,35 @@
+# Public: Enables tap to click on trackpad
+#
+# Examples
+#
+#   include osx::global::enable_tap_to_click
+class osx::global::enable_tap_to_click {
+
+  boxen::osx_defaults { 'Enable tap to click on bluetooth trackpad':
+    ensure => present,
+    domain => 'com.apple.driver.AppleBluetoothMultitouch.trackpad',
+    key    => 'Clicking',
+    value  => true,
+    type   => bool,
+    user   => $::boxen_user;
+  }
+
+  boxen::osx_defaults { 'Enable tap to click on login screen':
+    ensure => present,
+    host   => 'currentHost',
+    domain => 'NSGlobalDomain',
+    key    => 'Clicking',
+    value  => 1,
+    type   => int,
+    user   => $::boxen_user;
+  }
+
+  boxen::osx_defaults { 'Enable tap to click for user':
+    ensure => present,
+    domain => 'NSGlobalDomain',
+    key    => 'Clicking',
+    value  => 1,
+    type   => int,
+    user   => $::boxen_user;
+  }
+}
