716c1acf423e539e0517e30c15be36ec934a0c0e
Dominic Graefen <dominic.graefen@gmail.com>
2013-03-19 23:38:27 +0100

    Disabling bluetooth trackpad for now

--- manifests/global/enable_tap_to_click.pp
+++ manifests/global/enable_tap_to_click.pp
@@ -5,14 +5,14 @@
 #   include osx::global::enable_tap_to_click
 class osx::global::enable_tap_to_click {
 
-  boxen::osx_defaults { 'Enable tap to click on bluetooth trackpad':
-    ensure => present,
-    domain => 'com.apple.driver.AppleBluetoothMultitouch.trackpad',
-    key    => 'Clicking',
-    value  => true,
-    type   => bool,
-    user   => $::boxen_user;
-  }
+  #boxen::osx_defaults { 'Enable tap to click on bluetooth trackpad':
+  #  ensure => present,
+  #  domain => 'com.apple.driver.AppleBluetoothMultitouch.trackpad',
+  #  key    => 'Clicking',
+  #  value  => true,
+  #  type   => bool,
+  #  user   => $::boxen_user;
+  #}
 
   #boxen::osx_defaults { 'Enable tap to click on login screen':
   #  ensure => present,
